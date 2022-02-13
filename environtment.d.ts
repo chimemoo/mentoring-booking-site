dc8092adb659ff5c2a235144c1ee5e73eaac1673
Chimemoo <christmemory.sitorus@stockbit.com>
2022-02-13 16:50:58 +0700

    fix auth

--- environtment.d.ts
+++ environtment.d.ts
@@ -2,7 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       GOOGLE_CLIENT_ID: string; // this is the line you want
-      GOOGLE_CLIENT_SECRET: string;
+      SECRET: string;
       NEXT_PUBLIC_API_URL: string;
       NODE_ENV: 'development' | 'production';
       PORT?: string;
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 const clientId = process.env.GOOGLE_CLIENT_ID;
-const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+const clientSecret = process.env.SECRET;
 
 export default NextAuth({
   providers: [
